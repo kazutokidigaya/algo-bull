eec5cd6f65ef6152ad5fcded9af205781d926a61
karanT <2001karanpt@gmail.com>
2024-12-05 18:33:39 +0530

    Prepare for Railway deployment

--- algobulls/settings.py
+++ algobulls/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,8 @@ REST_FRAMEWORK = {
     ],
 }
 
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
+ALLOWED_HOSTS = ['*']
